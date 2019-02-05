22b7c6d4dfa0abbace931d9680b207d3239a6d81
root <root@e530.localdomain>
2019-02-05 00:35:54 -0500

    fixed composer make - again

--- .utils/composer.sh
+++ .utils/composer.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
     bash ./.utils/message.sh info "Using composer container - this can be very slow..."
     bash ./.utils/message.sh info "Please consider installing composer locally: https://getcomposer.org/download/"
+    cd web_root
     docker -H tcp://localhost:2375 run --rm --interactive --tty \
         --volume $PWD/web_root:/app/bedrock \
         composer /bin/bash -c "composer $@"
